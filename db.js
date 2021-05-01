9918d8f6b3aec90c73a6b9b5fd4b0523255c3bca
Jerom Ghagra <Jeromghagra@gmail.com>
2021-05-01 16:05:16 +0600

    intial commit

---
+++ db.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const winston = require('winston')
+const config = require('config')
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [],
+});
+
+//for testing db
+async function Connect(params) {
+    return await mongoose.connect(config.get('db'),{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useFindAndModify:false,
+        useCreateIndex:true,
+    })
+}
+const db = config.get('db')
+Connect(db)
+    .then(()=>console.log('Conneted to '+db))
+    .catch(err=>console.log(err))
+
+
+/* async function Connect(params) {
+    return await mongoose.connect('mongodb://localhost/vidly',{
+        useNewUrlParser:true,
+        useUnifiedTopology:true,
+        useFindAndModify:false,
+        useCreateIndex:true,
+    })
+} 
+
+Connect()
+    .then(()=>console.log('Conneted to MongoDB..'))
+    .catch(err=>console.log(err))
+*/
---
+++ startup/logging.js
@@ -0,0 +1,27 @@
+require('express-async-errors')
+const winston = require('winston')
+
+module.exports = function() {
+    const logger = winston.createLogger({
+        level: 'info',
+        format: winston.format.json(),
+        defaultMeta: { service: 'user-service' },
+        transports: [  
+          new winston.transports.Console(),
+          new winston.transports.File({ filename: 'combined.log' }),
+        ],
+    });
+    
+    //require('winston-mongodb')
+    //Add log to database
+    //winston.add(new winston.transports.MongoDB({db:'mongodb://localhost/vidly' }))
+    
+    process.on('uncaughtException', (ex)=>{
+        console.log('We got an uncaught exception.')
+        logger.error(ex.message, ex)
+    })
+    
+    process.on('unhandledRejection', (ex)=>{
+        throw ex
+    })
+}
